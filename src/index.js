3958353c6fa34d26d227022175cc16fcf35686b5
Hunter Jefferson <hj@artkade.com>
2018-11-20 21:49:09 -0600

    Added App and SeasonDisplay component skeletons.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
 
 // const App = () => {
 //   return (
