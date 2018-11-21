691c71cb62a64cf56ed9af9fce02e245b0fa3318
Hunter Jefferson <hj@artkade.com>
2018-11-20 21:06:52 -0600

    Setting up Seasons app.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import faker from 'faker';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
 
+// const App = () => {
+//   return (
+//     <div className="ui container comments">
+//       <ApprovalCard>
+//         <CommentDetail author="Sam" />
+//       </ApprovalCard>
+//     </div>
+//   );
+// };
+
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail author="Sam" />
-      </ApprovalCard>
-    </div>
-  );
+  return <div className="ui container comments">Seasons</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
