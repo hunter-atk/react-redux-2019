f49553bd39bebe7bb0adc3006e96eb5c5c2a6aee
Hunter Jefferson <hj@artkade.com>
2018-11-23 18:56:09 -0600

    App handles errors and renders error message on screen.

--- src/index.js
+++ src/index.js
@@ -15,18 +15,30 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
-      err => console.log(err)
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    return (
-      <div className="ui container comments">Latitude: {this.state.lat}</div>
-    );
+    if (this.state.lat && !this.state.errorMessage) {
+      return (
+        <div className="ui container comments">Latitude: {this.state.lat}</div>
+      );
+    } else if (!this.state.lat && this.state.errorMessage) {
+      return (
+        <div>
+          <div className="ui container comments">
+            Error: {this.state.errorMessage}
+          </div>
+        </div>
+      );
+    } else {
+      return <div className="ui container comments">Loading...</div>;
+    }
   }
 }
 
