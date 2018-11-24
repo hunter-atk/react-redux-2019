1607ec41377b4e5adb1076dabd0b41601906e173
Hunter Jefferson <hj@artkade.com>
2018-11-23 22:28:58 -0600

    Season icons render on screen with season text.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat >= 0 ? 'SUMMER' : 'WINTER';
+    return lat >= 0 ? 'summer' : 'winter';
   } else {
-    return lat >= 0 ? 'WINTER' : 'SUMMER';
+    return lat >= 0 ? 'winter' : 'summer';
   }
 };
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  return <div>{season}</div>;
+  const text =
+    season === 'summer' ? "Let's hit the beach!" : "Buurrr...It's chilly!";
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
