d54115d2729b4693a01da46c75e5cd8ff3835f2e
Hunter Jefferson <hj@artkade.com>
2018-11-23 21:32:26 -0600

    Fixed bug.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SeasonDisplay = props => {
   if (
     (props.lat >= 0 && (props.month >= 3 && props.month <= 8)) ||
-    (props.lat <= 0 && (props.month < 3 && props.month > 8))
+    (props.lat <= 0 && (props.month < 3 || props.month > 8))
   ) {
     return <div>It's summertime!</div>;
   }
